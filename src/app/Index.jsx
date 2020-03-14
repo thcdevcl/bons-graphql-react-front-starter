48836ace9ce8a321b169a94c6f21614c07aab7b6
Diego Robles <diego@hemployeed.com>
2020-03-13 22:22:26 -0300

    feat(app):add postcss and bons project structure

--- src/app/Index.jsx
+++ src/app/Index.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./styles/main.css";
+
+import logo from "./shared/images/logo.svg";
 
 function App() {
   return (
